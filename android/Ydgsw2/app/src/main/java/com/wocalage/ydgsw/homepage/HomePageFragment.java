d8ae96ffcb37a4a59ca97a3e29c9ec5faaa05e86
wocalage <1650507438@qq.com>
2017-08-11 15:46:38 +0800

    add some file
    rt

---
+++ android/Ydgsw2/app/src/main/java/com/wocalage/ydgsw/homepage/HomePageFragment.java
@@ -0,0 +1,10 @@
+package com.wocalage.ydgsw.homepage;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jiaojian on 2017/8/11.
+ */
+
+public class HomePageFragment extends Fragment {
+}
---
+++ android/Ydgsw2/app/src/main/java/com/wocalage/ydgsw/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wocalage.ydgsw.main;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.wocalage.ydgsw.R;
+
+/**
+ * Created by jiaojian on 2017/8/11.
+ */
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
